95369a739799a21b73a91e3c7c1f8589cdcaa814
Bosbrosnab <abowes@unomaha.edu>
2024-09-01 15:13:40 -0500

    Update FirstProgram.py

--- FirstProgram.py
+++ FirstProgram.py
@@ -1,22 +1,24 @@
 #FirstProgram.py
-#Name:
-#Date:
-#Assignment:
+#Name: Alex Bowes
+#Date: 9/1/24
+#Assignment: Lab 1
 
 def main():
   print("First Program")
   #Say hello
-  
+  print("hello")
   #Ask for the user's name
-
+  name=input("Put your name here: ")
   #Use the user's name in the program.
-
+  print("hello",name)
   #Ask the user for their age.
-
+  age=int(input("Put your age here: "))
   #Tell the user what year they were born in.
+  print(2024-age)
   #Assume that they have not had their birthday yet this year.
 
 
 #Call the main function if this is the file being run.
 if __name__ == '__main__':
     main()
+
