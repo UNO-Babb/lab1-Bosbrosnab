849cd1876bab1b79b266d725176adca0d2c174f4
Bosbrosnab <abowes@unomaha.edu>
2024-09-02 10:05:21 -0500

    Update MadLib.py

--- MadLib.py
+++ MadLib.py
@@ -1,16 +1,23 @@
 #MadLib.py
-#Name:
-#Date:
-#Assignment:
+#Name: Alex Bowes
+#Date: 9/1/24
+#Assignment: Lab 1
 
 def main():
-  print("Madlib")
-  #Ask user for words
+ print('hello and welcome to MadLibs!')
+name1 = input('please put any name here: ')
+age1 = input('now put an age for a person here: ')
+color1 = input('type your favorite color: ')
+hobby1 = input('please type one of your favorite hobbies: ')
+city1 = input('now type the name of any city: ')
+job1 = input('type your dream job!: ')
+food1 = input('now type your favorite food here: ')
+pet1 = input('please type your favorite animal: ')
+verb1 = input('now give me a verb: ')
+noun1 = input('and one noun: ')
+sport1 = input('almost done! now type your favorite sport: ')
+print('One day', name1, 'was', verb1, 'through the local park of', city1, 'where they saw a rare', pet1, ' that was loose! But because', name1, 'was a skilled', job1, 'they were able to capture the', pet1, 'and put it in a nearby', noun1, 'where it would be contained. After that wild chase,', name1, 'saw a nearby poster and realized that their favorite sport', sport1, 'was going to be playing tonight!', name1, 'quickly left the park and drove to the store to pick up their favorite game snack,', food1, 'and watch the', sport1, 'game. When', name1, 'got home, they rushed to the couch and fumbled for the TV remote, and when they found it the TV would not turn on because the TV was', age1, 'years old!', name1, 'thankfully got the TV working, but the TV was only showing the game in', color1, '!', name1, 'eventually got so fed up that they decided that they were just going to turn on a show about their favorite hobby', hobby1, 'while they ate their', food1, 'and relaxed.')
 
-  #Print the story with the user supplied words.
 
-
-
-#Call the main function if this is the file being run.
 if __name__ == '__main__':
     main()
